346abf291fc3d335f3ffd5a187987567a1ac1aa6
amoldesai8 <45632121+amoldesai8@users.noreply.github.com>
2020-10-27 18:27:03 +0530

    Update scaleset.tf

--- scaleset.tf
+++ scaleset.tf
@@ -7,12 +7,7 @@ resource "azurerm_virtual_machine_scale_set" "demo" {
   automatic_os_upgrade = true
   upgrade_policy_mode  = "Rolling"
 
-  rolling_upgrade_policy {
-    max_batch_instance_percent              = 20
-    max_unhealthy_instance_percent          = 20
-    max_unhealthy_upgraded_instance_percent = 5
-    pause_time_between_batches              = "PT0S"
-  }
+
 
   # required when using rolling upgrade policy
   health_probe_id = azurerm_lb_probe.demo.id
